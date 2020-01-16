194f13a2e3c22a9ed453e4516d508033612cd688
StudentA <cathychen.123456@gmail.com>
2020-01-16 18:39:34 +0800

    Main Page 大概樣子

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <template>
   <v-app>
     <v-container class="d-flex justify-center">
-      <FormBackground />
       <router-view></router-view>
     </v-container>
   </v-app>
 </template>
 
 <script>
-import FormBackground from '@/components/FormBackground';
+
 export default {
   name: 'App',
   components: {
-    FormBackground
+
   },
 
   data: () => ({
--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,34 +1,123 @@
 <template>
   <v-app>
     <v-card
-     height="500"
+     height="1000"
     >
-      <v-card-title class="text-center justify-center py-6">
-        <h1 class="font-weight-bold display-3 basil--text">FRC 7636</h1>
-      </v-card-title>
+      <v-card
+       color="#002a5c">
+        <v-img
+        aspect-ratio="3"
+        contain
+        src="../assets/Frontimg.jpg"
+        >
+        </v-img>        
+      </v-card>
       <v-tabs
         v-model="tab"
-        background-color="#00002d"
+        background-color="#00001d"
         grow
       >
-        <v-tab
-          v-for="item in links"
-          :key="item"
-        >
-          {{ item }}
+        <v-tab>
+          Home
+        </v-tab>
+
+        <v-tab>
+          <v-menu
+          open-on-hover
+          down
+          offset-y
+          >
+            <template v-slot:activator="{ on }">
+              <v-btn
+              v-on="on"
+              >
+                About
+              </v-btn>
+            </template>
+            <v-list>
+              <v-list-item>
+                <v-list-item-title >About us</v-list-item-title>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-title>History</v-list-item-title>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-title>Calender</v-list-item-title>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-title>Contact Us</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>          
+        </v-tab>
+
+        <v-tab>
+          FIRST
+        </v-tab>
+
+        <v-tab>
+          <v-menu
+          open-on-hover
+          down
+          offset-y
+          >
+            <template v-slot:activator="{ on }">
+              <v-btn
+              v-on="on"
+              >
+                Robots
+              </v-btn>
+            </template>
+            <v-list>
+              <v-list-item>
+                <v-list-item-title >HandBook</v-list-item-title>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-title>CAD</v-list-item-title>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-title>Code</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>          
+        </v-tab>
+        <v-tab>
+          <v-menu
+          open-on-hover
+          down
+          offset-y
+          >
+            <template v-slot:activator="{ on }">
+              <v-btn
+              v-on="on"
+              >
+                Media
+              </v-btn>
+            </template>
+            <v-list>
+              <v-list-item>
+                <v-list-item-title >Photos</v-list-item-title>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-title>Videos</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>          
+        </v-tab>
+
+        <v-tab>
+          Sponsors
+        </v-tab>
+
+        <v-tab>
+          login
         </v-tab>
       </v-tabs>
 
       <v-tabs-items v-model="tab">
-        <v-tab-item
-          v-for="item in links"
-          :key="item"
-        >
-          <v-card flat>
-            <v-card-text>{{ text }}</v-card-text>
-          </v-card>
-        </v-tab-item>
+
       </v-tabs-items>
+      
     </v-card>
     <v-footer padless>
       <v-col
@@ -45,14 +134,6 @@
     data () {
       return {
         tab: null,
-        links: [
-          'Home',
-          'About Us',
-          'Social Media',
-          'support us',
-          'login'
-        ],
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
       }
     },
   }
