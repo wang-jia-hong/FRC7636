62470702794813724d6b27c0c685b85e46a5bd80
StudentA <cathychen.123456@gmail.com>
2020-01-17 12:34:17 +0800

    Main Page 大概樣子

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,123 +1,104 @@
 <template>
-  <v-app>
-    <v-card
-     height="1000"
-    >
-      <v-card
-       color="#002a5c">
+  <div>
+    <v-card height="1000">
+      <!-- 封面圖 -->
+      <v-card color="#002a5c">      
         <v-img
-        aspect-ratio="3"
-        contain
-        src="../assets/Frontimg.jpg"
+          aspect-ratio="3"
+          contain
+          src="../assets/Frontimg.jpg"
         >
         </v-img>        
       </v-card>
+
       <v-tabs
+        color="success"
         v-model="tab"
-        background-color="#00001d"
+        background-color="transparent"
         grow
       >
-        <v-tab>
-          Home
-        </v-tab>
+        <v-tab class="white--text"> Home </v-tab>
 
-        <v-tab>
-          <v-menu
+        <v-menu
+          class="align-self-center mr-4 white--text"
           open-on-hover
           down
           offset-y
-          >
-            <template v-slot:activator="{ on }">
-              <v-btn
+        >
+          <template v-slot:activator="{ on }">
+            <v-btn
+              text
+              color="white"
+              class="align-self-center mr-4 white--text"
               v-on="on"
-              >
-                About
-              </v-btn>
-            </template>
-            <v-list>
-              <v-list-item>
-                <v-list-item-title >About us</v-list-item-title>
-              </v-list-item>
-              <v-list-item>
-                <v-list-item-title>History</v-list-item-title>
-              </v-list-item>
-              <v-list-item>
-                <v-list-item-title>Calender</v-list-item-title>
-              </v-list-item>
-              <v-list-item>
-                <v-list-item-title>Contact Us</v-list-item-title>
-              </v-list-item>
-            </v-list>
-          </v-menu>          
-        </v-tab>
+            >
+              About
+              <v-icon right>mdi-menu-down</v-icon>
+            </v-btn>
+          </template>
+          <v-list>
+            <v-list-item @click="" v-for="(aboutItem, index) in aboutItems" :key="index">
+              <v-list-item-title>{{ aboutItem }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>          
 
-        <v-tab>
-          FIRST
-        </v-tab>
+        <v-tab class="white--text"> FIRST </v-tab>
 
-        <v-tab>
-          <v-menu
+        <v-menu
           open-on-hover
           down
           offset-y
-          >
-            <template v-slot:activator="{ on }">
-              <v-btn
+        >
+          <template v-slot:activator="{ on }">
+            <v-btn
+              class="align-self-center mr-4 white--text"
+              text
               v-on="on"
-              >
-                Robots
-              </v-btn>
-            </template>
-            <v-list>
-              <v-list-item>
-                <v-list-item-title >HandBook</v-list-item-title>
-              </v-list-item>
-              <v-list-item>
-                <v-list-item-title>CAD</v-list-item-title>
-              </v-list-item>
-              <v-list-item>
-                <v-list-item-title>Code</v-list-item-title>
-              </v-list-item>
-            </v-list>
-          </v-menu>          
-        </v-tab>
-        <v-tab>
-          <v-menu
+            >
+              Robots
+              <v-icon right>mdi-menu-down</v-icon>
+            </v-btn>
+          </template>
+          <v-list>
+            <v-list-item v-for="(robotItem, index) in robotItems" :key="index">
+              <v-list-item-title>{{ robotItem }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>          
+
+        <v-menu
           open-on-hover
           down
           offset-y
-          >
-            <template v-slot:activator="{ on }">
-              <v-btn
+        >
+          <template v-slot:activator="{ on }">
+            <v-btn
+              text
+              class="align-self-center mr-4 white--text"
               v-on="on"
-              >
-                Media
-              </v-btn>
-            </template>
-            <v-list>
-              <v-list-item>
-                <v-list-item-title >Photos</v-list-item-title>
-              </v-list-item>
-              <v-list-item>
-                <v-list-item-title>Videos</v-list-item-title>
-              </v-list-item>
-            </v-list>
-          </v-menu>          
-        </v-tab>
+            >
+              Media
+              <v-icon right>mdi-menu-down</v-icon>
+            </v-btn>
+          </template>
+          <v-list>
+            <v-list-item v-for="(mediaItem, index) in mediaItems" :key="index">
+              <v-list-item-title>{{ mediaItem }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
 
-        <v-tab>
-          Sponsors
-        </v-tab>
+        <v-tab class="white--text"> Sponsors </v-tab>
 
-        <v-tab>
-          login
-        </v-tab>
+        <v-tab class="white--text"> login </v-tab>
+
       </v-tabs>
 
       <v-tabs-items v-model="tab">
 
       </v-tabs-items>
-      
+
     </v-card>
     <v-footer padless>
       <v-col
@@ -126,14 +107,17 @@
       >
         {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
       </v-col>
-    </v-footer>    
-  </v-app>
+    </v-footer>     
+  </div>   
 </template>
 <script>
   export default {
     data () {
       return {
         tab: null,
+        aboutItems: ["About us", "History", "Calender", "Contact Us"],
+        robotItems: ["HandBook","CAD","Code"],
+        mediaItems: ["Photos", "Videos"],
       }
     },
   }
