35effdbf6a5ffd02f73d4e0c24380c587c8fd93a
StudentA <cathychen.123456@gmail.com>
2020-01-18 21:26:46 +0800

    修正

--- src/components/ToolBar.vue
+++ src/components/ToolBar.vue
@@ -6,104 +6,91 @@
         v-model="currentTab"
         background-color="#222222"
         grow
+        class="white--text"
       >
-        <v-tab class="white--text" :href="'#'"> Home </v-tab>
-
-        <v-menu
-          class="align-self-center mr-4 white--text"
-          open-on-hover
-          down
-          offset-y
-        >
-          <template v-slot:activator="{ on }">
-            <v-btn
-              text
-              color="white"
-              class="align-self-center mr-4 white--text"
-              v-on="on"
-            >
-              About
-              <v-icon right>mdi-menu-down</v-icon>
-            </v-btn>
-          </template>
-          <v-list>
-            <v-list-item  v-for="(aboutItem, index) in aboutItems" :key="index" :href="'#tabs-' + index">
-              <v-list-item-title>{{ aboutItem }}</v-list-item-title>
-            </v-list-item>
-          </v-list>
-        </v-menu>          
-
+        <v-tab :href="'#'"> Home </v-tab>
+        <v-tab>
+          <v-menu class="white--text" open-on-hover down offset-y>
+            <template v-slot:activator="{ on }">
+              <div
+                text
+                color="white"
+                class="align-self-center white--text"
+                v-on="on"
+              >
+                About
+                <v-icon>mdi-menu-down</v-icon>
+              </div>
+            </template>
+            <v-list>
+              <v-list-item
+                v-for="(aboutItem, index) in aboutItems"
+                :key="index"
+                :href="'#tabs-' + index"
+              >
+                <v-list-item-title>{{ aboutItem }}</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </v-tab>
         <v-tab class="white--text"> FIRST </v-tab>
 
-        <v-menu
-          open-on-hover
-          down
-          offset-y
-        >
-          <template v-slot:activator="{ on }">
-            <v-btn
-              class="align-self-center mr-4 white--text"
-              text
-              v-on="on"
-            >
-              Robots
-              <v-icon right>mdi-menu-down</v-icon>
-            </v-btn>
-          </template>
-          <v-list>
-            <v-list-item v-for="(robotItem, index) in robotItems" :key="index">
-              <v-list-item-title>{{ robotItem }}</v-list-item-title>
-            </v-list-item>
-          </v-list>
-        </v-menu>          
-
-        <v-menu
-          class="align-self-center mr-4 white--text"
-          open-on-hover
-          down
-          offset-y
-        >
-          <template v-slot:activator="{ on }">
-            <v-btn
-              text
-              class="align-self-center mr-4 white--text"
-              v-on="on"
-            >
-              Media
-              <v-icon right>mdi-menu-down</v-icon>
-            </v-btn>
-          </template>
-          <v-list>
-            <v-list-item v-for="(mediaItem, index) in mediaItems" :key="index">
-              <v-list-item-title>{{ mediaItem }}</v-list-item-title>
-            </v-list-item>
-          </v-list>
-        </v-menu>
-
+        <v-tab>
+          <v-menu open-on-hover down offset-y>
+            <template v-slot:activator="{ on }">
+              <v-btn class="align-self-center mr-4 white--text" text v-on="on">
+                Robots
+                <v-icon right>mdi-menu-down</v-icon>
+              </v-btn>
+            </template>
+            <v-list>
+              <v-list-item
+                v-for="(robotItem, index) in robotItems"
+                :key="index"
+              >
+                <v-list-item-title>{{ robotItem }}</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </v-tab>
+        <v-tab>
+          <v-menu
+            class="align-self-center mr-4 white--text"
+            open-on-hover
+            down
+            offset-y
+          >
+            <template v-slot:activator="{ on }">
+              <v-btn text class="align-self-center mr-4 white--text" v-on="on">
+                Media
+                <v-icon right>mdi-menu-down</v-icon>
+              </v-btn>
+            </template>
+            <v-list>
+              <v-list-item
+                v-for="(mediaItem, index) in mediaItems"
+                :key="index"
+              >
+                <v-list-item-title>{{ mediaItem }}</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </v-tab>
         <v-tab class="white--text"> Sponsors </v-tab>
-
       </v-tabs>
-
-      <v-tabs-items v-model="currentTab">
-          <v-tab-item
-        
-          ></v-tab-item>
-      </v-tabs-items>
-
-    </v-card>    
-  </div>   
+    </v-card>
+  </div>
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        currentTab: null,
-        aboutItems: ["About us", "History", "Calender", "Contact Us"],
-        robotItems: ["HandBook","CAD","Code"],
-        mediaItems: ["Photos", "Videos"],
-        
-      }
-    },
-  }
+export default {
+  data () {
+    return {
+      currentTab: null,
+      aboutItems: ["About us", "History", "Calender", "Contact Us"],
+      robotItems: ["HandBook", "CAD","Code"],
+      mediaItems: ["Photos", "Videos"],
+    }
+  },
+}
 </script>
