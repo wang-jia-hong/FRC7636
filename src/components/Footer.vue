cf66a16c773b036e8c616c199f2f00860947637f
StudentA <cathychen.123456@gmail.com>
2020-01-22 00:59:28 +0800

    footer icon add link

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
-四季紅, [19.01.20 17:17]
 <template>
   <v-footer dark padless>
     <v-row>
+
       <v-col class="text-center my-auto" v-for="(item, index) in items" :key="index">
         <h2>{{ item.title }}</h2>
         <hr class="mx-auto mb-5" width="150">
@@ -11,12 +11,14 @@
           </router-link>
         </h3>
       </v-col>
+
       <v-col class="text-center my-auto">
         <h2>Contact Us</h2>
-        <v-btn v-for="icon in icons" :key="icon" class="white-text" icon>
-          <v-icon size="24px">{{ icon }}</v-icon>
+        <v-btn v-for="icon in icons" :key="icon" class="white-text" :href="icon.path" target="_blank" icon>
+          <v-icon size="24px">{{ icon.title }}</v-icon>
         </v-btn>
       </v-col>
+
       <v-col>
         <v-img src="../assets/FIRST_logo.png" class="ma-3" width="130"></v-img>
       </v-col>
@@ -27,7 +29,24 @@
 <script>
 export default {
   data: () => ({
-    icons: ["mdi-facebook", "mdi-youtube", "mdi-instagram", "mdi-email"],
+    icons: [
+      {
+        title: "mdi-facebook",
+        path: "https://www.facebook.com/7636.Robomania" 
+      },
+      {
+        title: "mdi-youtube",
+        path: "https://www.youtube.com/channel/UCXFLKnRlpTNMn2aaLVM9H6g"         
+      },
+      {
+        title: "mdi-instagram",
+        path: "https://www.instagram.com/frc7636_robomania/"        
+      },
+      {
+        title: "mdi-email",
+        path: ""       
+      },
+    ],
     items: [
       {
         title: "About",
