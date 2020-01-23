5cba1789612c6e32cadf385cd57adb744b96509d
StudentA <cathychen.123456@gmail.com>
2020-01-23 22:46:03 +0800

    homePage update

--- src/views/Contents/HomePage.vue
+++ src/views/Contents/HomePage.vue
@@ -6,30 +6,52 @@
         >
         </v-img>
         <v-card 
-        class="pa-10"
+        class="pa-10 align-content-space-around flex-wrap d-flex justify-center"
         color="#A1A29E"
-        height="500">
-            <v-card color="#767774" height="420">
-                <v-row class="mb-8">
-                    <v-col cols="8">
-                        <h1>About US</h1>
-                        <div>文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案</div>
-                    </v-col>
-                    <v-col>這裡是圖片的位置喔喔喔喔喔喔</v-col>
-                </v-row>
-                <v-row class="mb-8">
-                    <v-col>這裡是圖片的位置喔喔喔喔喔喔</v-col>                    
-                    <v-col cols="8">
-                        <h1>FRC 2019</h1>
+        height="1000">
+            <v-card color="#767774" height="220" class="d-flex align-center">
+
+                <v-row>
+                    <v-col lg="3" md="3" sm="12" class="text-center">
+                        <img src="../../assets/Elements/Home/Home-CTSP.png" width="100">
+                    </v-col>                    
+                    <v-col>
+                        <h1>NEHS @ CTSP</h1>
                         <div>文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案</div>
                     </v-col>
                 </v-row>
-                <v-row class="mb-8">
-                    <v-col cols="8">
-                        <h1>Sponsors</h1>
+
+            </v-card>
+            <v-card color="#767774" height="420" class=" d-flex flex-column align-center">
+
+                <v-row>
+                    <v-col>
+                        <h1>NEHS @ CTSP</h1>
                         <div>文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案</div>
                     </v-col>
-                    <v-col>這裡是圖片的位置喔喔喔喔喔喔</v-col>
+                    <v-col lg="3" md="3" sm="12" class="text-center">
+                        <img src="../../assets/Elements/Home/Home-CTSP.png" width="100">
+                    </v-col>                     
+                </v-row>
+
+                <v-row>
+                    <v-col lg="3" md="3" sm="12" class="text-center">
+                        <img src="../../assets/Elements/Home/Home-CTSP.png" width="100">
+                    </v-col>                    
+                    <v-col>
+                        <h1>NEHS @ CTSP</h1>
+                        <div>文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案</div>
+                    </v-col>
+                </v-row>
+
+                <v-row>
+                    <v-col>
+                        <h1>NEHS @ CTSP</h1>
+                        <div>文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案</div>
+                    </v-col>
+                    <v-col lg="3" md="3" sm="12" class="text-center">
+                        <img src="../../assets/Elements/Home/Home-CTSP.png" width="100">
+                    </v-col>                     
                 </v-row>
             </v-card>
         </v-card>
