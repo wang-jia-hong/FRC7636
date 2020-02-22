61322ba7611732c9a17ee0df2ab5b0ba89ac0be1
StudentA <cathychen.123456@gmail.com>
2020-02-23 02:30:26 +0800

    改成 hash 模式

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/FRC7636' : './',
+  publicPath: process.env.NODE_ENV === 'production' ? '/FRC7636/' : '/',
   "transpileDependencies": [
     "vuetify"
   ],
