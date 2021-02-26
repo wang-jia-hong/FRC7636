e6e316ec1df3ab029243028fde5c38d5c1b31012
Yu_Yue <9987jdvrb@gmail.com>
2021-02-26 19:17:30 +0800

    auto commit

--- commit.sh
+++ commit.sh
@@ -1,5 +1,5 @@
 #must commit!
 git add .
 git commit . -i -m "auto commit"
-git push https://github.com/SJZ99/SJZ99.github.io.git master
+git push -f https://github.com/SJZ99/SJZ99.github.io.git master
 read -n 1 -p "Press any key to continue..."
